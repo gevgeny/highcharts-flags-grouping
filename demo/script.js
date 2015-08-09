12d25cdfe54b1f555871185f69756b574a87f56f
gevgeny <e.gluhotorenko@gmai.com>
2015-08-09 15:36:19 +0300

    initial

---
+++ demo/script.js
@@ -0,0 +1,109 @@
+$(function() {
+    var series = [{
+        name : 'MSFT',
+        id : "msft",
+        data : window.prices
+    },{
+        type : "flags",
+        data : window.flags,
+
+        onSeries :"msft",
+        color : '#28AE7A'
+    }];
+    //series.push(window.flags);
+    $('#container').highcharts('StockChart', {
+        tooltip : {
+            positioner: function (w, h, point) {
+                var position = this.getPosition(w, h, point);
+
+                if(this.chart.flagTooltip) {
+                    position.y -= 40;
+                }
+
+                return position;
+            },
+            useHTML : true,
+            followPointer : true,
+            formatter : function (tooltip) {
+                if (this.point && this.point.initialPoints && this.point.initialPoints.length > 1) {
+                    var text = '<b>' + Highcharts.dateFormat('%b %d, %Y', this.point.initialPoints[0].x) + '</b>' +
+                        '<span> - </span>' +
+                        '<b>' + Highcharts.dateFormat('%b %d, %Y', this.point.initialPoints[this.point.initialPoints.length - 1].x) + '</b><br/>';
+
+                    text += '<span style="font-size: 11px">'
+                    for (var i = 0; i < this.point.initialPoints.length; i++) {
+                        text += '<p style="width: 400px;white-space: nowrap;overflow: hidden;text-overflow: ellipsis;">- ' + this.point.initialPoints[i].text + '</p>'
+                        if (i === 5) {
+                            text += '<p>...</p>';
+                            break
+                        }
+                    }
+
+                    return text + '</span>';
+                } else if (this.point) {
+                    return '' +
+                        '<b>' + Highcharts.dateFormat('%b %d, %Y', this.point.x) + '</b><br/>' +
+                        '<span>' + this.point.text + '</span>';
+
+                }
+                return tooltip.defaultFormatter.apply(this, [tooltip]);
+            }
+
+        },
+        plotOptions : {
+            series : {
+                turboThreshold: 2000
+            },
+            flags: {
+                shape :"circlepin",
+                style: {
+                    cursor: 'pointer',
+                    fontWeight: 'bold',
+                    textAlign: 'center'
+                },
+                events: {
+                    mouseOver: function(){
+                        this.chart.flagTooltip = true;
+                    },
+                    mouseOut: function(){
+                        this.chart.flagTooltip = false;
+                    }
+                },
+                point: {
+                    events: {
+                        click: function () {
+                            if (this.initialPoints) {
+                                this.series.xAxis.setExtremes(this.initialPoints[0].x, this.initialPoints[this.initialPoints.length - 1].x, true, true);
+                            }
+                        }
+                    }
+                }
+            }
+        },
+        flagsGrouping : {
+            calculateFillColor : true,
+            groupings : [{
+                zoomTimeSpan : 2 * 365 * 24 * 60 * 60 * 1000, // when more then 2 years selected
+                groupTimeSpan :     60 * 24 * 60 * 60 * 1000  // group flags by 60 days
+            },{
+                zoomTimeSpan :     365 * 24 * 60 * 60 * 1000, // when from 2 to 1 years selected
+                groupTimeSpan :     30 * 24 * 60 * 60 * 1000  // group by 30 days
+            }, {
+                zoomTimeSpan :     182 * 24 * 60 * 60 * 1000, // when from 1 to half year selected
+                groupTimeSpan :     15 * 24 * 60 * 60 * 1000  // group flags by 15 days
+            }, {
+                zoomTimeSpan :      90 * 24 * 60 * 60 * 1000, // when from half year to 3 month selected
+                groupTimeSpan :      5 * 24 * 60 * 60 * 1000  // group by 5 days
+            }]
+        },
+
+        title : {
+            text : 'MSFT Stock Price'
+        },
+
+        series : series
+    });
+});
+
+
+
---
+++ flags-grouping.js
@@ -0,0 +1,251 @@
+/**
+ * This plugin adds possibility to group flags series in accordance with selected time range
+ * */
+(function (H) {
+    var baseSetDataMethod;
+    /**
+     * key - chart serie
+     * value - points lists
+     * */
+    var pointsGroups = new Map();
+
+    /**
+     * key - chart serie
+     * value - current number of grouping that applied to the serie
+     * */
+    var seriesGropingNumbers = new Map();
+
+    /**
+     * key - chart
+     * value - chart options
+     * */
+    var chartsOpts = new Map();
+
+    var rgbToHex = function(r, g, b) {
+        var componentToHex = function(c) {
+            var hex = c.toString(16);
+            return hex.length === 1 ? "0" + hex : hex;
+        };
+        return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+    };
+
+    var hexToRgb = function(hex) {
+        var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+        return [
+            parseInt(result[1], 16),
+            parseInt(result[2], 16),
+            parseInt(result[3], 16)
+        ];
+    };
+
+    var calcPointBetweenTwo = function (start, end, offset) {
+        offset = (end - start) * offset;
+
+        return ~~(start + offset);
+    };
+
+    var calcColorsMix = function (from, to, offset) {
+        from = hexToRgb(from);
+        to = hexToRgb(to);
+
+        var r = calcPointBetweenTwo(from[0], to[0], offset);
+        var g = calcPointBetweenTwo(from[1], to[1], offset);
+        var b = calcPointBetweenTwo(from[2], to[2], offset);
+
+        return rgbToHex(r, g, b);
+    };
+
+    var applyColors = function (pointsLists, baseGroupFillColor) {
+        var maxCountOfPoints = 0;
+
+        pointsLists.forEach(function (points) {
+            var maxCountOfPoints = 0;
+
+            points.forEach(function (point) {
+                if (point.initialPoints.length > maxCountOfPoints) {
+                    maxCountOfPoints = point.initialPoints.length;
+                }
+            });
+
+            points.forEach(function (point) {
+                point.fillColor = calcColorsMix('#FFFFFF', baseGroupFillColor, point.initialPoints.length / maxCountOfPoints);
+            });
+        });
+
+    };
+
+    /**
+     * Group points in accordance with specified grouping options
+     * */
+    var groupPoints = function (serie, points, opts, baseGroupFillColor) {
+        var groupings = opts.groupings;
+
+        // Create list of groups for every provided grouping.
+        var pointsLists = groupings.map(function (grouping, index) {
+            var timeSpan = grouping.groupTimeSpan;
+            var resultGroupedPoints = [];
+            var newGroupPoints = [];
+            var newGroupPosition = 0;
+
+            // Calculate end date for the first group
+            var groupEndDate = points[0].x + timeSpan;
+
+            // Splitting points to groups where each group corresponds to
+            // an appropriate date range with length limited by the timeSpan.
+            for (var i = 0 ; i < points.length; i++) {
+                // Collect points in one group
+                newGroupPoints.push(points[i]);
+
+                // Sum group points positions to calculate avg
+                newGroupPosition += points[i].x;
+
+                // Close the group if the next point does not belong to this group or does not exists at all.
+                if (!points[i + 1] || points[i + 1].x > groupEndDate) {
+                    // Calculate avg position for group point
+                    newGroupPosition /= newGroupPoints.length;
+
+                    // Create a point for the group
+                    resultGroupedPoints.push({
+                        x : newGroupPosition,
+                        title : newGroupPoints.length,
+                        text : 'group: ' + index + 'flags: ' + newGroupPoints.length,
+                        initialPoints : newGroupPoints
+                    });
+
+                    // Reset current group
+                    newGroupPoints = [];
+                    newGroupPosition = 0;
+
+                    // Calculate end date for the next group if the next points exits
+                    if (points[i + 1]) {
+                        groupEndDate = points[i + 1].x + timeSpan;
+                    }
+                }
+            }
+            return resultGroupedPoints;
+        });
+
+        if (opts.calculateFillColor) {
+            applyColors(pointsLists, baseGroupFillColor);
+        }
+
+        // last grouping is initial set of points
+        pointsLists.push(points);
+
+        pointsGroups.set(serie, {
+            isAddedToChart : false,
+            pointsLists : pointsLists
+        });
+
+    };
+
+    /**
+     * Get appropriate number of points group in accordance with provided time span and options
+     * */
+    var getAppropriateGroupNumber = function (chart, selectedTimeSpan) {
+        var groupings = chartsOpts.get(chart).groupings;
+
+        // Use the first grouping if provided time stamp is more than any specified in options
+        if (selectedTimeSpan > groupings[0].zoomTimeSpan) {
+            return 0;
+        }
+
+        // Chose list of points in accordance with provided time span and options
+        for (var i = 1; i < groupings.length; i++) {
+            if (selectedTimeSpan <= groupings[i - 1].zoomTimeSpan && selectedTimeSpan > groupings[i].zoomTimeSpan) {
+                return i;
+            }
+        }
+
+        // Use the original points (without any grouping) if provided time stamp is less that any specified in options
+        return groupings.length;
+    };
+
+    var replaceSeriePoints = function (serie, newPoints) {
+        if (serie.points) {
+            baseSetDataMethod.apply(serie, [newPoints, false, false]);
+        } else {
+            newPoints.forEach(function (point) {
+                serie.addPoint(point, false);
+            });
+        }
+    };
+
+    /**
+     * Use this hook to fetch data series and calculate grouped points
+     * */
+    H.wrap(H.Series.prototype, 'setData', function (proceed, points) {
+        var opts = this.chart.options.flagsGrouping;
+        if (this.type === 'flags' && opts) {
+            groupPoints(this, arguments[1], opts, this.color);
+            chartsOpts.set(this.chart, opts);
+
+            // Init the serie with empty data since atm we don't know what date range is selected and what grouping should be used
+            arguments[1] = [];
+        }
+
+        baseSetDataMethod = proceed;
+        proceed.apply(this, Array.prototype.slice.call(arguments, 1));
+
+    });
+
+    /**
+     * Use this hook to set set points with necessary grouping when serie inits
+     * */
+    H.wrap(H.Series.prototype, 'translate', function (proceed) {
+        var opts = this.chart.options.flagsGrouping;
+        var serie = this;
+
+        // Process only at first call of the hook
+        if (this.type === 'flags' && opts && !pointsGroups.get(this).isAddedToChart) {
+            // Get default data range
+            var extremes = this.xAxis.getExtremes();
+
+            var groupNumber = getAppropriateGroupNumber(this.chart, extremes.max - extremes.min);
+
+            // Remember current grouping
+            seriesGropingNumbers.set(this, groupNumber);
+            //opts.selectedGroupNumber = groupNumber;
+
+            // Set points with appropriate grouping
+            var points = pointsGroups.get(serie).pointsLists[groupNumber];
+            replaceSeriePoints(serie, points);
+
+            // Mark the series is processed in order not to process it on more time
+            pointsGroups.get(this).isAddedToChart = true;
+        }
+
+        proceed.apply(this, Array.prototype.slice.call(arguments, 1));
+    });
+
+    /**
+     * Use this hook to set replace points with necessary grouping when date range changes
+     * */
+    H.wrap(H.Axis.prototype, 'setExtremes', function (proceed, min, max) {
+        var opts = this.chart.options.flagsGrouping,
+            chart = this.chart,
+            axis = this,
+            timeSpan = max - min;
+
+        if (opts) {
+            pointsGroups.forEach(function (pointsLists, serie) {
+                if (serie.chart !== chart || serie.xAxis !== axis || !pointsGroups.get(serie).isAddedToChart) {
+                    return;
+                }
+
+                var groupNumber = getAppropriateGroupNumber(chart, timeSpan);
+
+                // Process only if the date range changed enough to change grouping
+                if (seriesGropingNumbers.get(serie) !== groupNumber) {
+                    // Set points with appropriate grouping
+                    var points = pointsGroups.get(serie).pointsLists[groupNumber];
+                    replaceSeriePoints(serie, points);
+
+                    // Remember new group number for this serie
+                    seriesGropingNumbers.set(serie, groupNumber);
+                }
+            });
+        }
+        proceed.apply(this, Array.prototype.slice.call(arguments, 1));
+    });
+}(Highcharts));
